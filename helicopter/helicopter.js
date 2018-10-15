0757375d883b9686319d3c35f4a8a8415e78c986
Roger B <myvarequals@gmail.com>
2018-10-14 20:45:01 -0400

    starting helicopter game

---
+++ helicopter/helicopter.js
@@ -0,0 +1,22 @@
+//define globals
+
+// load img assets
+
+// draw background
+
+// draw foreground
+
+// draw heli
+
+// draw walls
+
+// move heli
+
+// move walls
+    // draw new walls as needed
+
+// detect collision
+    // reset on collision
+
+// track score
+
