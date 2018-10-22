b3f7a1cdaa14028746c2d943e44427c137583873
Roger B <myvarequals@gmail.com>
2018-10-21 20:26:59 -0400

    added comments because spaghetti, should seperate code

--- spaceshooter/spaceshooter.js
+++ spaceshooter/spaceshooter.js
@@ -28,6 +28,8 @@ explosionSheet.src = 'imgAssets/explosion.png';
 var expSheetW = 786;
 var expSheetH = 128;
 var expSpriteW = expSheetW / 7;
+
+// tests for game over
 var exploding = false;
 var gameOver = false;
 
@@ -56,7 +58,7 @@ function drawSpace() {
 function drawStar(x,y,size) {
     drawRect(x,y, size, size, 'white');
 }
-// fill star array
+// use to fill star array
 function populateStars() {
 
     for (i = 0; i < 20; i++) {
@@ -111,16 +113,17 @@ function BogieOne() {
 }
 // track bogies on screen
 var bogiesArray = [];
-
+// game level
 var level = 1;
 
+// use to fill bogies array
 function populateBogies() {
 
     for (i = 0; i < level; i++) {       
         bogiesArray[i] = new BogieOne();    
     }
 }
-
+// generate bogies
 populateBogies();
     
 
@@ -147,24 +150,29 @@ function moveBogies(bogies) {
     }
 }
 
+// array to push new lasers to
 var bogieLaserArray = [];
 
+//use to get all bogies shooting
 function generateBogieLasers(bogies) {
+    // for each bogie...
     for (i = 0; i < bogies.length; i++) {
+        // shoot a new laser
         bogieLaserArray.push(new Laser(bogies[i].x, bogies[i].y));                     
     }
-
+    // wait 2.5s, fire again...
     setTimeout(function(){
         generateBogieLasers(bogiesArray);
     },2500);
     
 }
-
+// start shooting
 generateBogieLasers(bogiesArray);
 
+// draw lasers from bogies
 function drawBogiesLasers(bogies) {
     for (i = 0; i < bogies.length; i++) {
-        if(!bogies[i]) {
+        if(!bogies[i]) { // if item in array was deleted, skip iteration
             continue;
         } 
         // console.log(bogieLaserArray);
@@ -176,24 +184,24 @@ function drawBogiesLasers(bogies) {
 
 function moveBogiesLasers(bogies) {
     for (i = 0; i < bogies.length; i++) {
-        if(!bogies[i]) {
+        if(!bogies[i]) { // if item in array was deleted, skip iteration
             continue;
         }      
         bogieLaserArray[i].y += 3;   
-        if(testCollision(bogies[i])) {                           
+        if(testCollision(bogies[i])) { // delete laser on collision                    
             delete bogieLaserArray[i];
             if (ship.hitpoints > 0) {
-                ship.hitpoints--;
+                ship.hitpoints--; // lose 1 hp
 
             }
-            if (ship.hitpoints == 0) {
-                gameOver = true;
+            if (ship.hitpoints == 0) { // 0 hp
+                gameOver = true; 
                 exploding = true;              
             }
             console.log(ship.hitpoints);
             continue;
         }
-        if (bogieLaserArray[i].y > canvas.height) {
+        if (bogieLaserArray[i].y > canvas.height) { // delete laser off screen
             delete bogieLaserArray[i];
         }            
     }
@@ -210,16 +218,16 @@ function Laser(x,y) {
     this.y = y;
 }
 
+
 var keysPressed = {};
 addEventListener('keydown', function(e) {
-    keysPressed[e.keyCode] = true;
-
-    
+    keysPressed[e.keyCode] = true; // when pressing key, add to array
 });
 addEventListener('keyup', function(e) {
-    delete keysPressed[e.keyCode];
+    delete keysPressed[e.keyCode]; // delete on up
 });
 
+// hero laser
 function drawLasers() {
     for (i = 0; i < laserArray.length; i++) {
         if (laserArray[i] == undefined) {
@@ -228,7 +236,7 @@ function drawLasers() {
         ctx.drawImage(laserImg, laserArray[i].x, laserArray[i].y);
     }
 }
-
+// hero move laser
 function moveLasers() {
     for (i = 0; i < laserArray.length; i++) {
         if (laserArray[i] == undefined) {
@@ -241,6 +249,7 @@ function moveLasers() {
     }
 }
 
+// explosion animation
 function drawExplosion() { 
     console.log('drawExplosion()');
     let now = Date.now();
@@ -262,9 +271,10 @@ function drawExplosion() {
            
 }
 
+// test array for key being pressed
 Ship.prototype.controlShip = function(tick) {
 
-    if(32 in keysPressed) {         
+    if(32 in keysPressed) { // space, fire        
             this.count++;
         if(this.count == this.rate) {
             laserArray.push(new Laser(ship.x,ship.y));
@@ -272,22 +282,22 @@ Ship.prototype.controlShip = function(tick) {
         }      
         
     }    
-    if (37 in keysPressed) {
+    if (37 in keysPressed) { // left
         if(this.x > 0) {
             this.x -= this.v * tick;
         }
     }
-    if (38 in keysPressed) {
+    if (38 in keysPressed) { // up
         if (this.y > canvas.height / 2) {        
             this.y -= this.v * tick;
         }
     }
-    if (39 in keysPressed) {
+    if (39 in keysPressed) { // right
         if (this.x < canvas.width - heroW) {        
             this.x += this.v * tick;
         }
     }
-    if (40 in keysPressed) {
+    if (40 in keysPressed) { // down
         if (this.y < canvas.height - heroH) {
             this.y += this.v * tick;
         }
@@ -296,7 +306,8 @@ Ship.prototype.controlShip = function(tick) {
 
 function testCollision(laser) {
     var collision = false;
-    if(laser.x > ship.x &&
+    // if laser is within the size of the hero...
+    if(laser.x > ship.x && 
         laser.x < ship.x + heroW &&
         laser.y > ship.y &&
         laser.y < ship.y + heroH) {            
