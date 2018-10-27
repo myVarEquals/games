a4e2e9d8af21e31522517aa1ff86a43c7c65d02c
Roger B <myvarequals@gmail.com>
2018-10-27 01:41:52 -0400

    added collision edges, food

--- snake/snake.js
+++ snake/snake.js
@@ -5,6 +5,7 @@ const SNAKE_SIZE = 25; // 25px by 25px squares
 
 // initialize snake direction
 var direction = 'right';
+var gameOver = false;
 
 window.onload = function() { // when window loads...
     drawBackground();
@@ -51,20 +52,54 @@ document.addEventListener('keydown', function(e) {
         direction = 'down';
     }
 })
+console.log(cvs.width);
+food = {
+    x: Math.floor(Math.random() * (cvs.width/SNAKE_SIZE)), // rand x pos
+    y: Math.floor(Math.random() * (cvs.height/SNAKE_SIZE)) // rand y pos
+}
+// console.log(food);
 
-function draw() {
-    ctx.clearRect(0,0,cvs.width,cvs.height);
+function drawFood(x,y) { // draw apple
+    ctx.fillStyle = "red"; 
+    ctx.fillRect(x*SNAKE_SIZE, y*SNAKE_SIZE, SNAKE_SIZE, SNAKE_SIZE);
+                // dx, dy, w, h
+    ctx.fillStyle = "white";
+    ctx.strokeRect(x*SNAKE_SIZE, y*SNAKE_SIZE, SNAKE_SIZE, SNAKE_SIZE);
+}
 
+function draw() { // main draw
+    ctx.clearRect(0,0,cvs.width,cvs.height); // clear canv
+    drawBackground(); // black
     for (i = 0; i < snake.length; i++) { // for length of snake array
         var x = snake[i].x; // get args
         var y = snake[i].y; // ^ ^ ^ 
         drawSnake(x,y); // draw piece
     }
+
+    
+    drawFood(food.x, food.y); // food
     // snake head pos
     var snakeX = snake[0].x;
     var snakeY = snake[0].y;
 
-    snake.pop(); // remove tail
+    if (snakeX >= cvs.width / SNAKE_SIZE || // snake hits right
+        snakeX < 0 ||                       // snake hits left
+        snakeY >= cvs.height / SNAKE_SIZE ||// snake hits bottom
+        snakeY < 0) {                       // snake hits top
+            location.reload(); // reload page to reset game
+    }
+
+    if (snakeX == food.x && snakeY == food.y) { // snake head hits food
+        food = { // new food;
+            x: Math.floor(Math.random() * (cvs.width/SNAKE_SIZE)),
+            y: Math.floor(Math.random() * (cvs.height/SNAKE_SIZE))
+        }
+        // console.log(food);
+        // console.log(len);
+    } else {        
+        snake.pop(); // remove tail
+    }
+
 
     // check current direct of snake
     if (direction == 'left') {
@@ -87,4 +122,4 @@ function draw() {
     snake.unshift(newHead); // add new head to front of array
 }
 
-setInterval(draw, 60);
+setInterval(draw, 1000);
